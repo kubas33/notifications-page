fd4de95487c1429941ddd1c87cee7fdc4a5f6b2d
kubas33 <Stasiak91@gmail.com>
2023-06-20 22:45:33 +0200

    Refactor of script

--- js/script.js
+++ js/script.js
@@ -4,124 +4,110 @@ let counter = 0;
 const unreadNumber = document.getElementById("unread-num");
 
 function countUnread() {
-  [...notifList.children].forEach(li => {
-    if (li.classList.contains("unread"))
-      counter++;
-  })
+  counter = [...notifList.children].reduce((acc, li) => {
+    return li.classList.contains("unread") ? acc + 1 : acc;
+  }, 0);
   unreadNumber.textContent = counter;
 }
 
 function readNotification(event) {
   let notification = event.target.closest("li");
-  console.log("click");
    if (notification.classList.contains("unread")) {
     notification.classList.remove("unread");
-    counter--;
     countUnread();
    }
 }
 
-function addNotifications(data) {
-  const notifications = [...data];
+function createNotificationElement(notificationData) {
+  const li = document.createElement("li");
+  li.dataset.author = notificationData.notification.author.name;
+  li.addEventListener("click", readNotification);
+  const notifAuthor = document.createElement("strong");
+  notifAuthor.textContent = notificationData.notification.author.name;
+  const link = document.createElement("a");
+  const content = document.createElement("div");
+  const message = document.createElement("p");
+  const picture = document.createElement("img");
 
-  notifications.forEach(e => {
-    const li = document.createElement("li");
-    const space = document.createTextNode(" ");
-    li.dataset.author = e.notification.author.name;
-    if (!e.notification.read) {
-      li.classList.add("unread");
-    }
+  if (!notificationData.notification.read) {
+    li.classList.add("unread");
+  }
+  const photo = document.createElement("img");
+  photo.src = notificationData.notification.author.photo;
+  photo.classList.add("photo");
+  photo.alt = "";
+  li.appendChild(photo);
 
-    const notificationElement = document.createElement("p");
-    const notifAuthor = document.createElement("strong");
-    const link = document.createElement("a");
-    const picture = document.createElement("img");
-    const message = document.createElement("p");
-    const photo = document.createElement("img");
-    const content = document.createElement("div");
-    const time = document.createElement("time");
+  const notificationElement = document.createElement("p");
+  const notifText = document.createTextNode('\u00A0' + notificationData.notification.message + '\u00A0');
+  notificationElement.appendChild(notifAuthor);
+  notificationElement.appendChild(notifText);
 
-    notifAuthor.textContent = e.notification.author.name;
-    const notifText = document.createTextNode('\u00A0' + e.notification.message + '\u00A0');
-    photo.src = e.notification.author.photo;
-    photo.classList.add("photo");
-    photo.alt = "";
-    li.appendChild(photo);
+  switch(notificationData.notification.type) {
+  case "post":
+    li.classList.add("post");
+    link.textContent = notificationData.notification.target;
+    link.href = "#";
+    notificationElement.appendChild(link);
+    break;
+  case "follow":
+    li.classList.add("follow");
+    break;
+  case "group":
+    li.classList.add("group");
+    link.textContent = notificationData.notification.target;
+    link.href = "#";
+    notificationElement.appendChild(link);
+    break;
+  case "message":
+    li.classList.add("message");
+    message.textContent = notificationData.notification.target;
+    message.classList.add("msg")
+    break;
+  case "picture":
+    li.classList.add("picture");
+    picture.src = notificationData.notification.target.src;
+    picture.alt = notificationData.notification.target.alt;
+    break;
+  }
 
-    notificationElement.appendChild(notifAuthor);
-    notificationElement.appendChild(notifText);
+  content.appendChild(notificationElement);
+  const time = document.createElement("time");
+  content.appendChild(time);
 
+  time.textContent = notificationData.notification.time;
+  if (notificationData.notification.type === "message")
+    content.appendChild(message);
+  else if (notificationData.notification.type === "picture")
+    content.appendChild(picture);
 
+  li.appendChild(content);
+  notifList.appendChild(li);
 
-
-    switch(e.notification.type) {
-      case "post":
-        li.classList.add("post");
-        link.textContent = e.notification.target;
-        link.href = "#";
-        notificationElement.appendChild(link);
-        break;
-      case "follow":
-        li.classList.add("follow");
-        break;
-      case "group":
-        li.classList.add("group");
-        link.textContent = e.notification.target;
-        link.href = "#";
-        notificationElement.appendChild(link);
-        break;
-      case "message":
-        li.classList.add("message");
-        message.textContent = e.notification.target;
-        message.classList.add("msg")
-        break;
-      case "picture":
-        li.classList.add("picture");
-        picture.src = e.notification.target.src;
-        picture.alt = e.notification.target.alt;
-        break;
-    }
-
-
-    time.textContent = e.notification.time;
-    content.appendChild(notificationElement);
-    content.appendChild(time);
-
-    if (e.notification.type === "message")
-      content.appendChild(message);
-    else if (e.notification.type === "picture")
-      content.appendChild(picture);
-
-      li.appendChild(content);
-      notifList.appendChild(li);
-  })
-
-  countUnread();
-
-  
 }
 
-fetch('../js/authors.json')
+function addNotifications(data) {
+  data.forEach(notification => {
+    createNotificationElement(notification);
+  })
+  countUnread();
+}
+
+fetch("../js/authors.json")
   .then(response => response.json())
   .then(data => {
     addNotifications(data);
-    //console.log(authors);
   })
   .catch(error => {
-    console.error('Wystąpił błąd podczas pobierania pliku JSON:', error);
+    console.error("Wystąpił błąd podczas pobierania pliku JSON:", error);
   });
 
 
 
-  btnRead.addEventListener("click", e => {
+  btnRead.addEventListener("click", () => {
     [...notifList.children].forEach(li => {
       li.classList.remove("unread");
     })
     counter = 0;
     countUnread();
-  })
-
-  Array.from(notifList.children).forEach(el => {
-    console.log(el);
-    el.addEventListener("click", readNotification);
   })
